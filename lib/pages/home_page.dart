82d36a14c63c20e060bd36999b556dce8252823f
Zayar Tun <kyawzayartun.dev@gmail.com>
2022-03-26 20:35:22 +0630

    initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/custom_animated_bottom_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final _inactiveColor = Colors.grey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        title: const Text('Custom Bottom Navigation Bar'),
+        backgroundColor: Colors.purple[200],
+      ),
+      body: _buildBody(),
+      bottomNavigationBar: _buildBottomBar(),
+    );
+  }
+
+  Widget _buildBody() {
+    List<Widget> pages = [
+      Container(
+        alignment: Alignment.center,
+        child: const Text(
+          'Home',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      Container(
+        alignment: Alignment.center,
+        child: const Text(
+          'Users',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      Container(
+        alignment: Alignment.center,
+        child: const Text(
+          'Messages',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      Container(
+        alignment: Alignment.center,
+        child: const Text(
+          'Settings',
+          style: TextStyle(
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    ];
+
+    return IndexedStack(
+      index: _currentIndex,
+      children: pages,
+    );
+  }
+
+  Widget _buildBottomBar() {
+    return CustomAnimatedBottomBar(
+      containerHeight: 70,
+      backgroundColor: Colors.black,
+      selectedIndex: _currentIndex,
+      showElevation: true,
+      itemCornerRadius: 24,
+      curve: Curves.easeIn,
+      onItemSelected: (index) => setState(() => _currentIndex = index),
+      items: <BottomNavyBarItem>[
+        BottomNavyBarItem(
+          icon: const Icon(Icons.apps),
+          title: const Text('Home'),
+          activeColor: Colors.green,
+          inactiveColor: _inactiveColor,
+          textAlign: TextAlign.center,
+        ),
+        BottomNavyBarItem(
+          icon: const Icon(Icons.people),
+          title: const Text('Users'),
+          activeColor: Colors.purpleAccent,
+          inactiveColor: _inactiveColor,
+          textAlign: TextAlign.center,
+        ),
+        BottomNavyBarItem(
+          icon: const Icon(Icons.message),
+          title: const Text('Messages'),
+          activeColor: Colors.pink,
+          inactiveColor: _inactiveColor,
+          textAlign: TextAlign.center,
+        ),
+        BottomNavyBarItem(
+          icon: const Icon(Icons.settings),
+          title: const Text('Settings'),
+          activeColor: Colors.blue,
+          inactiveColor: _inactiveColor,
+          textAlign: TextAlign.center,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/widgets/custom_animated_bottom_bar.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+
+class CustomAnimatedBottomBar extends StatelessWidget {
+  const CustomAnimatedBottomBar({
+    Key? key,
+    this.selectedIndex = 0,
+    this.iconSize = 24,
+    this.backgroundColor,
+    this.showElevation = true,
+    this.animationDuration = const Duration(milliseconds: 270),
+    required this.items,
+    required this.onItemSelected,
+    this.mainAxisAlignment = MainAxisAlignment.spaceBetween,
+    this.itemCornerRadius = 50,
+    this.containerHeight = 56,
+    this.curve = Curves.linear,
+  })  : assert(items.length >= 2 && items.length <= 5),
+        super(key: key);
+
+  final int selectedIndex;
+  final double iconSize;
+  final Color? backgroundColor;
+  final bool showElevation;
+  final Duration animationDuration;
+  final List<BottomNavyBarItem> items;
+  final ValueChanged<int> onItemSelected;
+  final MainAxisAlignment mainAxisAlignment;
+  final double itemCornerRadius;
+  final double containerHeight;
+  final Curve curve;
+
+  @override
+  Widget build(BuildContext context) {
+    final bgColor = backgroundColor ?? Theme.of(context).bottomAppBarColor;
+
+    return Container(
+      decoration: BoxDecoration(color: bgColor, boxShadow: [
+        if (showElevation)
+          const BoxShadow(
+            color: Colors.black12,
+            blurRadius: 2,
+          )
+      ]),
+      child: SafeArea(
+        child: Container(
+          width: double.infinity,
+          height: containerHeight,
+          padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 8),
+          child: Row(
+            mainAxisAlignment: mainAxisAlignment,
+            children: items.map((item) {
+              var index = items.indexOf(item);
+              return GestureDetector(
+                onTap: () => onItemSelected(index),
+                child: _ItemWidget(
+                  item: item,
+                  iconSize: iconSize,
+                  isSelected: index == selectedIndex,
+                  backgroundColor: bgColor,
+                  itemCornerRadius: itemCornerRadius,
+                  animationDuration: animationDuration,
+                  curve: curve,
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ItemWidget extends StatelessWidget {
+  final double iconSize;
+  final bool isSelected;
+  final BottomNavyBarItem item;
+  final Color backgroundColor;
+  final double itemCornerRadius;
+  final Duration animationDuration;
+  final Curve curve;
+
+  const _ItemWidget({
+    Key? key,
+    required this.iconSize,
+    required this.isSelected,
+    required this.item,
+    required this.backgroundColor,
+    required this.itemCornerRadius,
+    required this.animationDuration,
+    required this.curve,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Semantics(
+      container: true,
+      selected: isSelected,
+      child: AnimatedContainer(
+        width: isSelected ? 130 : 50,
+        height: double.maxFinite,
+        duration: animationDuration,
+        curve: curve,
+        decoration: BoxDecoration(
+          color:
+              isSelected ? item.activeColor.withOpacity(0.2) : backgroundColor,
+          borderRadius: BorderRadius.circular(itemCornerRadius),
+        ),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          physics: const NeverScrollableScrollPhysics(),
+          child: Container(
+            width: isSelected ? 130 : 50,
+            padding: const EdgeInsets.symmetric(horizontal: 8),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                IconTheme(
+                  data: IconThemeData(
+                    size: iconSize,
+                    color: isSelected
+                        ? item.activeColor.withOpacity(1)
+                        : item.inactiveColor ?? item.activeColor,
+                  ),
+                  child: item.icon,
+                ),
+                if (isSelected)
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 4),
+                      child: DefaultTextStyle.merge(
+                        child: item.title,
+                        style: TextStyle(
+                          color: item.activeColor,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        maxLines: 1,
+                        textAlign: item.textAlign,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BottomNavyBarItem {
+  final Widget icon;
+  final Widget title;
+  final Color activeColor;
+  final Color? inactiveColor;
+  final TextAlign? textAlign;
+
+  BottomNavyBarItem({
+    required this.icon,
+    required this.title,
+    this.activeColor = Colors.blue,
+    this.inactiveColor,
+    this.textAlign,
+  });
+}
